24ed11f1400308e5460a8b92e5c0f0802d483d2e
jlo-dcjd <91139882+jlo-dcjd@users.noreply.github.com>
2021-11-12 10:18:09 -0600

    Update programs.py

--- apps/programs.py
+++ apps/programs.py
@@ -39,81 +39,6 @@ def get_unique_numbers(numbers):
     return sorted(list_of_unique_numbers)
 
 
-def pro_chart_picker(op):
-    fig = make_subplots(rows=2, cols=1, shared_yaxes=True)
-
-    fig.add_trace(
-        go.Scatter(x=monthsfy, y=program.iloc[:12, op], name='FY 2020'),
-        row=1, col=1
-    )
-
-    fig.add_trace(
-        go.Scatter(x=monthsfy, y=program.iloc[12:24, op], name='FY 2021'),
-        row=1, col=1
-    )
-
-    fig.add_trace(
-        go.Bar(x=monthsfy, y=np.round(percentage_change(program.iloc[:12, prg_df_index].values, program.iloc[12:24, prg_df_index].values), 2), name='% Change 2020 vs 2021'),
-        row=2, col=1
-    )
-    fig.update_layout(title_text="{} - Youth Served".format(option))
-    return st.plotly_chart(fig)
-
-
-def pro_chart_exits(op):
-    fig = make_subplots(rows=3, cols=1, shared_yaxes=True, subplot_titles=("Total & Successful Exits", "Total Exits FY20 vs FY21", "Percent Change FY20 vs FY21"))
-
-    fig.add_trace(
-        go.Scatter(x=program.index, y=program.iloc[:, op], name='Total Exits'),
-        row=1, col=1
-    )
-
-    fig.add_trace(
-        go.Bar(x=program.index, y=program.iloc[:, op+1], name='Succ. Exits',  marker = {'color' : 'green'}),
-        row=1, col=1
-    )
-
-    fig.add_trace(
-        go.Bar(x=program.index, y=program.iloc[:, op + 2], name='Unsucc. Exits',  marker = {'color' : 'red'}),
-        row=1, col=1
-    )
-
-    fig.add_trace(
-        go.Scatter(x=monthsfy, y=program.iloc[:12, op], name='FY 2020'),
-        row=2, col=1
-    )
-
-    fig.add_trace(
-        go.Scatter(x=monthsfy, y=program.iloc[12:24, op], name='FY 2021'),
-        row=2, col=1
-    )
-
-    fig.add_trace(
-        go.Bar(x=monthsfy, y=np.round(percentage_change(program.iloc[:12, prg_df_index_te].values, program.iloc[12:24, prg_df_index_te].values), 2), name='% Change 20 vs 21'),
-        row=3, col=1
-    )
-    fig.update_layout(title_text="{} - Exits".format(option))
-    return st.plotly_chart(fig)
-
-
-
-
-def pro_chart_avg(op):
-    fig = make_subplots(rows=2, cols=1, shared_yaxes=True)
-
-
-    fig.add_trace(
-        go.Bar(x=program.index, y=program.iloc[:, op], name='ADP'),
-        row=1, col=1
-    )
-
-    fig.add_trace(
-        go.Bar(x=program.index, y=program.iloc[:, op + 1], name='ALOS'),
-        row=2, col=1
-    )
-
-    fig.update_layout(title_text="{} - ADP & ALOS".format(option))
-    return st.plotly_chart(fig)
 
 
 program = pd.read_csv('https://raw.githubusercontent.com/jlo-dcjd/juv-dashboard/main/apps/data/program_population.csv', index_col=0)
@@ -124,6 +49,87 @@ def app():
     st.title('Monthly Program Population')
 
     option = st.selectbox('Select program', pg_list)
+   
+   
+   
+    def pro_chart_picker(op):
+        fig = make_subplots(rows=2, cols=1, shared_yaxes=True)
+
+        fig.add_trace(
+            go.Scatter(x=monthsfy, y=program.iloc[:12, op], name='FY 2020'),
+            row=1, col=1
+        )
+
+        fig.add_trace(
+            go.Scatter(x=monthsfy, y=program.iloc[12:24, op], name='FY 2021'),
+            row=1, col=1
+        )
+
+        fig.add_trace(
+            go.Bar(x=monthsfy, y=np.round(percentage_change(program.iloc[:12, prg_df_index].values, program.iloc[12:24, prg_df_index].values), 2), name='% Change 2020 vs 2021'),
+            row=2, col=1
+        )
+        fig.update_layout(title_text="{} - Youth Served".format(option))
+        return st.plotly_chart(fig)
+
+
+    def pro_chart_exits(op):
+        fig = make_subplots(rows=3, cols=1, shared_yaxes=True, subplot_titles=("Total & Successful Exits", "Total Exits FY20 vs FY21", "Percent Change FY20 vs FY21"))
+
+        fig.add_trace(
+            go.Scatter(x=program.index, y=program.iloc[:, op], name='Total Exits'),
+            row=1, col=1
+        )
+
+        fig.add_trace(
+            go.Bar(x=program.index, y=program.iloc[:, op+1], name='Succ. Exits',  marker = {'color' : 'green'}),
+            row=1, col=1
+        )
+
+        fig.add_trace(
+            go.Bar(x=program.index, y=program.iloc[:, op + 2], name='Unsucc. Exits',  marker = {'color' : 'red'}),
+            row=1, col=1
+        )
+
+        fig.add_trace(
+            go.Scatter(x=monthsfy, y=program.iloc[:12, op], name='FY 2020'),
+            row=2, col=1
+        )
+
+        fig.add_trace(
+            go.Scatter(x=monthsfy, y=program.iloc[12:24, op], name='FY 2021'),
+            row=2, col=1
+        )
+
+        fig.add_trace(
+            go.Bar(x=monthsfy, y=np.round(percentage_change(program.iloc[:12, prg_df_index_te].values, program.iloc[12:24, prg_df_index_te].values), 2), name='% Change 20 vs 21'),
+            row=3, col=1
+        )
+        fig.update_layout(title_text="{} - Exits".format(option))
+        return st.plotly_chart(fig)
+
+
+
+
+    def pro_chart_avg(op):
+        fig = make_subplots(rows=2, cols=1, shared_yaxes=True)
+
+
+        fig.add_trace(
+            go.Bar(x=program.index, y=program.iloc[:, op], name='ADP'),
+            row=1, col=1
+        )
+
+        fig.add_trace(
+            go.Bar(x=program.index, y=program.iloc[:, op + 1], name='ALOS'),
+            row=2, col=1
+        )
+
+        fig.update_layout(title_text="{} - ADP & ALOS".format(option))
+        return st.plotly_chart(fig)
+   
+   
+   
 
     # column index: 9=youth served 9+2=t. exits
     prg_df_index = [y[0] for y in prog_en].index(option) * 9

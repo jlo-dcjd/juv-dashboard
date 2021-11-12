5c3d0cd4a32f5660508387afd94272202ff27766
jlo-dcjd <91139882+jlo-dcjd@users.noreply.github.com>
2021-11-12 10:07:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, roc, ref_perc, det_perc, det_boxplot, form_ref_paper_ref  # import your app modules here
+from apps import home, roc, ref_perc, det_perc, det_boxplot, form_ref_paper_ref, programs  # import your app modules here
 
 
 app = MultiApp()
@@ -18,6 +18,7 @@ app.add_app("Referral Offense % Change", ref_perc.app)
 app.add_app("Detention % Change", det_perc.app)
 app.add_app("Detention Boxplot per Year", det_boxplot.app)
 app.add_app("Formal & Paper Formalized Referrals", form_ref_paper_ref.app)
+app.add_app("Monthly Program Population", programs.app)
 
 
 # The main app
